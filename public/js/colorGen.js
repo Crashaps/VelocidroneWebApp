789eb25db5f9d0e9086580a01f9efe1ddf6e0fb2
Crashaps <crashaps@gmail.com>
2024-01-01 01:12:02 +0000

    added hexToRgb in colorGen.js

--- public/js/colorGen.js
+++ public/js/colorGen.js
@@ -47,3 +47,12 @@ function createRainbowDiv(start, end) {
     }
     return colors;
 }
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
