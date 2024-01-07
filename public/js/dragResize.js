810d0bee31f60a0556dc60960e981c138c451155
Crashaps <crashaps@gmail.com>
2024-01-07 20:16:53 +0000

    Updated z-indexing based on mouse down. Moved out replay functions to seperate js file

--- public/js/dragResize.js
+++ public/js/dragResize.js
@@ -17,21 +17,23 @@ interact('.miniwindow')
         listeners: {
             start: dragStartListener,
             move(event) {
-                let { x, y } = event.target.dataset;
-
-                x = parseFloat(x) || 0;
-                y = parseFloat(y) || 0;
+                let x = (parseFloat(event.target.getAttribute('data-x')) || 0) + ((event.deltaRect.left) ? event.deltaRect.left : 0);
+                let y = (parseFloat(event.target.getAttribute('data-y')) || 0) + ((event.deltaRect.top) ? event.deltaRect.top : 0);
 
                 Object.assign(event.target.style, {
                     width: `${event.rect.width}px`,
                     height: `${event.rect.height}px`,
-                    transform: `translate(${x}px, ${y}px)`
+                    transform: `translate3d(${x}px, ${y}px, 0px)`
                 });
+
+                event.target.setAttribute('data-x', x);
+                event.target.setAttribute('data-y', y);
             },
             end: (event) => { normalizeZIndexes(); savePosition(event.target); }
         },
         inertia: false
-    });
+    })
+    .on('down', dragStartListener);
 
 function dragMoveListener(event) {
     var target = event.target;
@@ -61,8 +63,17 @@ function dragStartListener(event) {
     const allWindows = document.querySelectorAll('.miniwindow');
     const zIndexes = Array.from(allWindows).map(el => parseInt(window.getComputedStyle(el).zIndex, 10) || 0);
     const currentMaxZIndex = zIndexes.length > 0 ? Math.max(...zIndexes) : 0;
+    let miniwindow = getParentMiniWindow(event.target);
+    miniwindow.style.zIndex = currentMaxZIndex + 1;
+}
 
-    event.target.style.zIndex = currentMaxZIndex + 1;
+function getParentMiniWindow(element) {
+    let parentElement = element;
+    while (parentElement && parentElement.classList.contains('miniwindow') == false) {
+        parentElement = parentElement.parentElement;
+    }
+
+    return parentElement;
 }
 
 function normalizeZIndexes() {
